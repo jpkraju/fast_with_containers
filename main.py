9757051ebf75d7e87b760fe6624c1dacfbc79060
Raju K <jp.k.raju@gmail.com>
2024-10-01 08:02:09 +0000

    message changes for testing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # main.py
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"He1llo": "World !!"}
+    return {"He1llo": "World..."}
+
+@app.get("/test")
+def get_test():
+    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    return {"test_on": dt_string}
